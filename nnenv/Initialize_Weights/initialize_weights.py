b5c119cc1477e2e33ebaedc20ce9240568ca3ae4
procode7 <kr.praveen399@gmail.com>
2019-04-26 00:06:37 +0530

    main.py

--- nnenv/Initialize_Weights/initialize_weights.py
+++ nnenv/Initialize_Weights/initialize_weights.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-class BasicNuralNetwork(Object):
+class BasicNuralNetwork(object):
     def __init__(self):
         np.random.seed(1234)
         self.rand_weight = []
--- nnenv/Train/train.py
+++ nnenv/Train/train.py
@@ -4,9 +4,9 @@ from Predict import  predict as pred
 from Sigmoid_Function import sigmoid as sig
 
 
-def train(self, file, X, y, iterations):
+def train(file, X, y, iterations):
     dim = file.shape
-    ini.BasicNuralNetwork.rand_weight = 2 * np.random.random(dim[1] - 1 ,1) -1
+    ini.BasicNuralNetwork.rand_weight = 2 * np.random.random((dim[1] - 1 ,1)    ) -1
 
     for i in range(iterations):
         output = pred.predict(X)
